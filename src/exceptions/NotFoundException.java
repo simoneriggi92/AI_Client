9ab30acaeb0159077fa280d4ba3a710f81c17cd5
Alberto <Alberto>
2018-04-11 09:53:50 +0200

    NotFoundException modificata

--- src/exceptions/NotFoundException.java
+++ src/exceptions/NotFoundException.java
@@ -4,10 +4,10 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-public class NotFoundException extends WebApplicationException {
+public class NotFoundException extends Throwable {
 
     public NotFoundException(String message) {
-        super(Response.status(Response.Status.fromStatusCode(404)).entity(message)
-                .type(MediaType.TEXT_PLAIN).build());
+        //super(Response.status(Response.Status.fromStatusCode(404)).entity(message)
+          //      .type(MediaType.TEXT_PLAIN).build());
     }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,5 @@
 module HttpClient{
-    requires gson;
+//    requires gson;
     requires java.json;
     requires jdk.incubator.httpclient;
     requires javax.ws.rs;
